d3db934e9ee4d5703e67d8b64d169cc39e7a7db4
jagephart <gepharja@miamioh.edu>
2020-08-06 16:30:49 -0400

    Add clean_livelihoods script

---
+++ clean_livelihoods.R
@@ -0,0 +1,24 @@
+# Livelihoods data cleaning
+# Raw data from Teh and Sumaila SI Table 1
+
+library(tidyverse)
+library(countrycode)
+
+# First load function for rebuilding from FishStat ZIP file:
+rm(list=ls())
+library(tidyverse)
+# note: Change outdir to the filepath where you want outputs to go
+outdir <- "Outputs"
+# for MacOS
+datadir <- "/Volumes/jgephart/Blue Foods/Data/Livelihoods Teh and Sumaila"
+
+df <- read.csv(file.path(datadir, "Teh_Sumaila_2013_SITable1.csv"))
+
+df$iso3c <- countrycode(df$Country, origin = "country.name", destination = "iso3c")
+df$iso3n <- countrycode(df$Country, origin = "country.name", destination = "iso3n")
+df$year.range <- "2003"
+
+df <- df %>%
+  select(country = Country, year.range, iso3c, iso3n, direct_w_esitimated_ssf, indirect_w_esitimated_ssf)
+
+write.csv(df, "Outputs/livelihoods_direct_indirect.csv")
