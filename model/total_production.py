eacb78c88ab7fb0acfa27b4f800452f2e2dd3823
Shin Nakayama <shinn1@stanford.edu>
2020-09-13 19:24:41 -0700

    Add summary stats

--- model/total_production.py
+++ model/total_production.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pymc3 as pm
 import theano.tensor as tt
 from plotnine import *
-
+import tqdm
 
 # function to standardize
 def standardize(x):
@@ -17,45 +17,37 @@ df.columns = df.columns.str.replace('.', '_')
 df = df[~df.iso3c.duplicated()]
 
 # variable names of interest
-x_name = ['mean_wage_gap_all_sectors', 'female_particip_ssf', 'mean_women_parl_perc']
 x_control = ['eez_total']
 y_name = ['mean_total_production']
 
 # prepare data
-X = df[x_name].copy()
+X = df.eez_total
+X.dropna(inplace=True)
+y = df.loc[X.index, 'mean_total_production']
 
-# drop rows where all predictors are missing
-X.dropna(how='all', inplace=True)
+#
+y = df.mean_total_production + 1
+y.dropna(inplace=True)
+X = df.loc[y.index, ['mean_wage_gap_all_sectors', 'female_particip_ssf', 'mean_women_parl_perc', 'eez_total']].copy()
+X.eez_total = X.eez_total.apply(np.log)
 
-# add control variables (log)
-control = df[x_control].apply(np.log, axis=0)
-X = X.merge(control, right_index=True, left_index=True)
-
-# standardize all
 X = X.apply(standardize, axis=0)
 
-# masked data for missing values
 X_masked = np.ma.masked_invalid(X)
-
-# observed values
-y = df.loc[X.index, y_var].values
-y = y + 1
-#y_masked = np.ma.masked_invalid(y)
 
 
 with pm.Model() as model:
     # priors
-    intercept = pm.Normal('intercept', mu=0, sigma=10)
-    beta = pm.Normal('beta', mu=0, sigma=10, shape=(X_masked.shape[1],))
-    eps = pm.HalfCauchy('eps', beta=5)
-    # eps = pm.Uniform('esp', lower=0, upper=1000)
+    intercept = pm.Normal('intercept', mu=0, sigma=100, testval=10)
+    beta = pm.Normal('beta', mu=0, sigma=10, testval=[-1,0,0,2], shape=(X_masked.shape[1],))
+    eps = pm.HalfCauchy('eps', beta=5, testval=2)
 
     # impute missing X
     X_mu = pm.Normal('X_mu', mu=0, sigma=10, shape=(1,X_masked.shape[1]))
-    X = pm.Normal('X', mu=X_mu, sigma=1, observed=X_masked)
+    X_modeled = pm.Normal('X', mu=X_mu, sigma=1, observed=X_masked)
 
     # observation
-    mu = intercept + tt.dot(X, beta)
+    mu = intercept + tt.dot(X_modeled, beta)
 
     # likelihood
     likelihood = pm.Lognormal('y', mu=mu, sigma=eps, observed=y)
@@ -65,10 +57,16 @@ with model:
     trace = pm.sample(1000)
 
 
+def summary_coeff(x):
+    foo = np.quantile(x, axis=0, q=[0.025, 0.5, 0.975])
+    foo = pd.DataFrame(np.transpose(foo))
+    foo.index = X.columns
+    foo.columns = ['lower', 'median', 'upper']
+    foo['significance'] = ['*' if np.logical_or(x1 > 0, x2 < 0) else '' for (x1, x2) in zip(foo.lower, foo.upper)]
+
+    return(foo)
+
+summary_coeff(trace.beta)
+
 pm.traceplot(trace)
 pm.summary(trace)
-
-
-foo = pm.summary(trace)
-beta = foo[['beta' in _ for _ in foo.index]]
-beta
