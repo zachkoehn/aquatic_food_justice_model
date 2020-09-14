3bb2ea38cf3c63e7082210308f836d6f3c6ad1ec
Shin Nakayama <shinn1@stanford.edu>
2020-09-14 14:09:44 -0700

    add more predictors

--- model/total_production.py
+++ model/total_production.py
@@ -1,13 +1,28 @@
+import scipy
 import numpy as np
 import pandas as pd
 import pymc3 as pm
 import theano.tensor as tt
+import matplotlib.pyplot as plt
+import seaborn as sns
 from plotnine import *
-import tqdm
+
 
 # function to standardize
 def standardize(x):
     return (x-np.mean(x))/np.std(x)
+
+# function to summarize samples
+def summary_coeff(x):
+    foo = np.quantile(x, axis=0, q=[0.025, 0.5, 0.975])
+    foo = pd.DataFrame(np.transpose(foo))
+    foo.index = X.columns
+    foo.columns = ['lower', 'median', 'upper']
+    foo['significance'] = ['*' if np.logical_or(x1 > 0, x2 < 0) else '' for (x1, x2) in zip(foo.lower, foo.upper)]
+
+    return(foo)
+
+
 
 # import data
 df = pd.read_csv('all_national_indicators.csv')
@@ -16,30 +31,49 @@ df.columns = df.columns.str.replace('.', '_')
 # remove duplicated countries
 df = df[~df.iso3c.duplicated()]
 
-# variable names of interest
-x_control = ['eez_total']
-y_name = ['mean_total_production']
 
 # prepare data
-X = df.eez_total
-X.dropna(inplace=True)
-y = df.loc[X.index, 'mean_total_production']
+y = df['mean_total_production'] / df['mean_population']
+y = y[y > 0].copy()
 
-#
-y = df.mean_total_production + 1
-y.dropna(inplace=True)
-X = df.loc[y.index, ['mean_wage_gap_all_sectors', 'female_particip_ssf', 'mean_women_parl_perc', 'eez_total']].copy()
-X.eez_total = X.eez_total.apply(np.log)
+x_cov = df[['mean_wage_gap_all_sectors', 'female_particip_ssf', 'mean_women_parl_perc',
+    'mean_pov_prop', 'mean_educ', 'mean_gdp',
+    'nb_languages_established', 'language_diversity', 'prop_pop_l1_inst',
+    'age_dep_ratio', 'mean_voice_account']]
 
+x_control = df[['eez_total', 'total_renewable_water', 'inland_water_min', 'inland_water_max']]
+
+X = x_cov.merge(x_control, left_index=True, right_index=True)
+X = X.loc[y.index, :].copy()
+
+# check distributions
+# sns.pairplot(X)
+
+# change distributions
+X[['mean_gdp', 'nb_languages_established', 'eez_total', 'total_renewable_water', 'inland_water_min', 'inland_water_max']] = \
+    X[['mean_gdp', 'nb_languages_established', 'eez_total', 'total_renewable_water', 'inland_water_min', 'inland_water_max']].apply(np.log, axis=0)
+
+# check correlations
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+plt.figure(figsize=(12,8))
+sns.heatmap(X.corr(),vmin=-1, vmax=1, annot=True, square=True, fmt='.2f', cmap=cmap)
+plt.savefig('correlations.pdf')
+
+# remove highly correlated variables
+X.drop(columns=['mean_pov_prop', 'mean_gdp', 'total_renewable_water', 'inland_water_min'], inplace=True)
+
+# standardize
 X = X.apply(standardize, axis=0)
 
+
+# mask NA
 X_masked = np.ma.masked_invalid(X)
 
 
 with pm.Model() as model:
     # priors
-    intercept = pm.Normal('intercept', mu=0, sigma=100, testval=10)
-    beta = pm.Normal('beta', mu=0, sigma=10, testval=[-1,0,0,2], shape=(X_masked.shape[1],))
+    intercept = pm.Normal('intercept', mu=0, sigma=100, testval=-4)
+    beta = pm.Normal('beta', mu=0, sigma=10, shape=(X_masked.shape[1],))
     eps = pm.HalfCauchy('eps', beta=5, testval=2)
 
     # impute missing X
@@ -54,19 +88,10 @@ with pm.Model() as model:
 
 
 with model:
-    trace = pm.sample(1000)
+    trace = pm.sample(4000, tune=2000)
 
-
-def summary_coeff(x):
-    foo = np.quantile(x, axis=0, q=[0.025, 0.5, 0.975])
-    foo = pd.DataFrame(np.transpose(foo))
-    foo.index = X.columns
-    foo.columns = ['lower', 'median', 'upper']
-    foo['significance'] = ['*' if np.logical_or(x1 > 0, x2 < 0) else '' for (x1, x2) in zip(foo.lower, foo.upper)]
-
-    return(foo)
 
 summary_coeff(trace.beta)
 
-pm.traceplot(trace)
-pm.summary(trace)
+# pm.traceplot(trace)
+# pm.summary(trace)
