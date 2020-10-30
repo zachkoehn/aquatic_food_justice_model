8a9d8274f3895831ce7dfff54480f717144dd709
Zach Koehn <zachkoehn@Zachs-MacBook-Pro.local>
2020-10-30 13:12:30 -0700

    replaced NAs with 0s just for FAO production data

--- Data Cleaning/merge_cleaned_data.R
+++ Data Cleaning/merge_cleaned_data.R
@@ -13,16 +13,28 @@ work_dir <- "/Volumes/GoogleDrive/My Drive/BFA_Papers/BFA_Justice/section_model/
 
 setwd(file.path(work_dir,"data","data_clean"))
 
-
-
 library(readr)  
 library(dplyr)
+
+# loads all files
 files<- list.files(pattern="*.csv")
 files <- files[files!="all_national_indicators.csv"] # remove pre-existing dataset so we aren't merging the same information on infinite repeat :) 
 df_list <- lapply(files, read_csv) 
+names(df_list) <- files
 
-head(df_list$nutrition_score_Fish_Invert.csv)
 
+# substitute 0 for NA values in all produciton data (no need to do this for total)
+df_list[["production_by_isccaap_faostat_mean_2006-2016.csv"]] <- df_list[["production_by_isccaap_faostat_mean_2006-2016.csv"]] %>%
+  mutate(
+    across(mean_prod_carps_etc:mean_pro_horeshoe_crabs_etc, ~replace_na(.x,0))
+    )
+
+df_list[["production_by_source_faostat_mean_2006-2016.csv"]] <- df_list[["production_by_source_faostat_mean_2006-2016.csv"]] %>%
+  mutate(
+    across(mean_aquaculture_production_freshwater:mean_aquaculture_production_brackish, ~replace_na(.x,0))
+    )
+
+# now collapse the list into a single data frame in order to export to CSV
 df_merged <- df_list %>%
   reduce(full_join, by = c("iso3n","iso3c")) %>% #merges all by the iso codes
   do(.[!duplicated(names(.))]) %>%
@@ -38,7 +50,11 @@ df_merged <- df_list %>%
          -Code #removes a code value that is from the voice and accountability (just a duplicated iso3c)
          
   ) %>% 
-  mutate(country_name_en=countrycode(iso3n,"iso3n","country.name.en")) %>%
+  mutate(
+    country_name_en=countrycode(iso3n,"iso3n","country.name.en"),
+    un_subregion_name=countrycode(iso3c,"iso3c","un.regionsub.name"),
+    un_subregion_code=countrycode(iso3c,"iso3c","un.regionsub.code")
+    ) %>%
   distinct() %>%
   mutate( # a few of the indicators reported their proportions as 57% instead of 0.57... so transforming those
     mean_educ=mean_educ*.01,
@@ -50,8 +66,8 @@ df_merged <- df_list %>%
     ) %>%
   dplyr::select(country_name_en,iso3c,iso3n,everything())
 
-
-
+summary(df_merged)
+ 
 
 write.csv(df_merged,
           file.path(work_dir,"data","data_clean","all_national_indicators.csv"),
