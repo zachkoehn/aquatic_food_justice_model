b75539b5834493924859ee9d2447a1a1cd25c1b2
Zach Koehn <zachkoehn@Zachs-MacBook-Pro.local>
2020-12-05 15:21:44 -0800

    added regional codes and modified a few variables

--- Data Cleaning/merge_cleaned_data.R
+++ Data Cleaning/merge_cleaned_data.R
@@ -19,6 +19,7 @@ library(dplyr)
 # loads all files
 files<- list.files(pattern="*.csv")
 files <- files[files!="all_national_indicators.csv"] # remove pre-existing dataset so we aren't merging the same information on infinite repeat :) 
+files <- files[files!="meow_climatic_zone.csv"] # remove pre-existing dataset so we aren't merging the same information on infinite repeat :) 
 df_list <- lapply(files, read_csv) 
 names(df_list) <- files
 
@@ -45,18 +46,39 @@ landlocked_countries <- oecd_categories$iso3c
 df_list[["production_by_isccaap_faostat_mean_2006-2016.csv"]] <- df_list[["production_by_isccaap_faostat_mean_2006-2016.csv"]] %>%
   mutate(
     across(mean_prod_carps_etc:mean_pro_horeshoe_crabs_etc, ~replace_na(.x,0))
-    )
+    ) %>%
+  select(-unit)
 df_list[["production_by_source_faostat_mean_2006-2016.csv"]] <- df_list[["production_by_source_faostat_mean_2006-2016.csv"]] %>%
   mutate(
     across(mean_aquaculture_production_freshwater:mean_aquaculture_production_brackish, ~replace_na(.x,0))
-    )
+    ) %>%
+  select(-unit)
+df_list[["production_total_faostat_mean_2006-2016.csv"]] <- df_list[["production_total_faostat_mean_2006-2016.csv"]] %>%
+  select(-unit)
 
+df_list[["aquastat_total_renewable_water.csv"]] <- df_list[["aquastat_total_renewable_water.csv"]] %>%  # name needs to be changed from year.range to year_range, also double 
+    rename(year_range=year.range) %>%
+    mutate(year_range=as.character(year_range))
+
+
+# name needs to be changed from year.range to year_range
+df_list[["FBS_seafood_consumption_reliance.csv"]] <- df_list[["FBS_seafood_consumption_reliance.csv"]] %>%  # name needs to be changed from year.range to year_range, also double 
+    rename(year_range=year.range) %>%
+    mutate(year_range=as.character(year_range))
+
+df_list[["voice_and_accountability_2008_2018.csv"]] <- df_list[["voice_and_accountability_2008_2018.csv"]] %>%  # name needs to be changed from year.range to year_range, also double 
+    rename(year_range=year.range) %>%
+    mutate(year_range=as.character(year_range))
+
+livelihoods_direct_indirect.csv
+df_list[["livelihoods_direct_indirect.csv"]] <- df_list[["livelihoods_direct_indirect.csv"]] %>%  # name needs to be changed from year.range to year_range, also double 
+    rename(year_range=year.range) %>%
+    mutate(year_range=as.character(year_range))
   # and now do the same thing for the SAU based variables that do not have landlocked countries
-
 
 # now collapse the list into a single data frame in order to export to CSV
 df_merged <- df_list %>%
-  reduce(full_join, by = c("iso3n","iso3c")) %>% #merges all by the iso codes
+  reduce(full_join,by=c("iso3c","iso3n")) %>% #merges all by the iso codes
   do(.[!duplicated(names(.))]) %>%
   filter(
     is.na(iso3n)==FALSE,
@@ -72,14 +94,24 @@ df_merged <- df_list %>%
   ) %>% 
   mutate(
     country_name_en=countrycode(iso3n,"iso3n","country.name.en"),
-    un_subregion_name=countrycode(iso3c,"iso3c","un.regionsub.name"),
-    un_subregion_code=countrycode(iso3c,"iso3c","un.regionsub.code")
+    un_region_name=countrycode(iso3n,"iso3n","un.region.name"),
+    un_region_code=countrycode(iso3n,"iso3n","un.region.code"),
+    un_subregion_1=countrycode(iso3c,"iso3c","un.regionsub.name"),
+    un_subregion_code=countrycode(iso3c,"iso3c","un.regionsub.code"),
+    un_subregion_finer_name=countrycode(iso3c,"iso3c","un.regionintermediate.name")
     ) %>%
   distinct() %>%
   mutate( # a few of the indicators reported their proportions as 57% instead of 0.57... so transforming those
     mean_educ=mean_educ*.01,
     mean_pov_prop=mean_pov_prop*.01,
-    mean_women_parl_perc=mean_women_parl_perc*.01
+    mean_women_parl_perc=mean_women_parl_perc*.01,
+    sat_model_est_wealth=1/sat_model_est_pov,
+    cultural_autonomy=1-cultural_hegemony,
+    gender_equality=1-mean_sigi,
+    fish_affordability=1/fish_relative_caloric_price,
+    un_subregion_2=ifelse(un_subregion_1=="Latin America and the Caribbean",un_subregion_finer_name,un_subregion_1), 
+    un_subregion_2=ifelse(un_subregion_2=="Sub-Saharan Africa",un_subregion_finer_name,un_subregion_2)
+
     ) %>%
   rename(
     mean_voice_account=mean.voice.and.accountability #and change variable name to underscore from period
@@ -87,10 +119,13 @@ df_merged <- df_list %>%
   dplyr::select(country_name_en,iso3c,iso3n,everything())
 
 
+
 # ok, now we need to replace  NA's for landlocked countries coming from SAU data with 0s (because SAU doesn't include any information on landlocked countries
 df_merged$eez_total[df_merged$iso3c %in% landlocked_countries] <- 0
 df_merged$ifa_total[df_merged$iso3c %in% landlocked_countries] <- 0
 df_merged$pp_eez_weighted[df_merged$iso3c %in% landlocked_countries] <- 0 
+
+
 
 write.csv(df_merged,
           file.path(work_dir,"data","data_clean","all_national_indicators.csv"),
